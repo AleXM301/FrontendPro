aaffce32a5bb73da62209c97bfcf504541d75387
vlad <sashkamarichok@gmail.com>
2025-02-19 23:38:48 +0200

    l3hw1

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let text = "lorem ";
+console.log(text);
+let number = 5;
+console.log(number);
+let boolean = true;
+console.log(boolean);
+let none = null;
+console.log(`${none}`);
+console.log(undefined);

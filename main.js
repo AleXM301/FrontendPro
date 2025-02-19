78d4558646787017c71200dbc15ce75147c16a5a
vlad <sashkamarichok@gmail.com>
2025-02-20 00:03:23 +0200

    l3hw2

---
+++ main.js
@@ -0,0 +1,4 @@
+let name = prompt('What is your name?');
+let year = prompt('What is your year?');
+let month = prompt('What is your month?');
+console.log(`Your name is ${name} and ${year} is ${month}`);

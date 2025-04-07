2c96281c89eb37c9bd7314c38c8a87db67922f2d
vlad <sashkamarichok@gmail.com>
2025-04-07 23:21:42 +0300

    l16hw1: fix after review

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ const formBtn = document.querySelector('.form__btn');
 const todosWrapper = document.querySelector('.js--todos-wrapper');
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    let newVar = JSON.parse(localStorage.getItem("tasks") || "[]");
-    newVar.forEach(item => renderList(item));
+    let tasks = getTasks();
+    tasks.forEach(item => addTask(item));
 });
 
+function getTasks() {
+    return JSON.parse(localStorage.getItem("tasks") || "[]");
+}
 
-function renderList(item) {
+function saveTasks(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function addTask(item) {
     const li = document.createElement('li');
     li.classList.add("todo-item");
     li.innerHTML =
-        `<input type="checkbox">
+        `<input class="checkbox" type="checkbox" data-id="${item.id}" ${item.checked ? "checked" : ""} >
          <span class="todo-item__description">${item.value}</span>
          <button class="todo-item__delete" data-id="${item.id}">Видалити</button>`;
     todosWrapper.appendChild(li);
@@ -21,40 +28,46 @@ function renderList(item) {
 formBtn.addEventListener('click', (e) => {
 
     e.preventDefault();
-    let valInput = input.value.trim();
-    if (!valInput) {
+    let value = input.value.trim();
+    if (!value) {
         alert("input is required");
         return;
     }
 
     let item = {
         id: Math.random(),
-        valInput
-    };
+        value,
+        checked: false
+    }
 
-    let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    let tasks = getTasks();
     tasks.push(item);
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    renderList(item);
+    saveTasks(tasks);
+    addTask(item);
 
     input.value = '';
 });
 
 todosWrapper.addEventListener("click", e => {
 
+    let tasks = getTasks();
     if (e.target.classList.contains(`todo-item__delete`)) {
 
         const id = +e.target.dataset.id;
-        let list = JSON.parse(localStorage.getItem("tasks") || "[]");
+        let filteredTask = tasks.filter(e => e.id !== id);
+        saveTasks(filteredTask)
 
-        let removeItem = list.filter(e => e.id !== id);
-        localStorage.setItem("tasks", JSON.stringify(removeItem));
+        e.target.closest('li').remove();
 
-        todosWrapper.innerHTML = "";
-        removeItem.forEach(item => renderList(item));
+    } else if (e.target.classList.contains("checkbox")) {
+        const id = +e.target.dataset.id;
+        let item = tasks.find(e => e.id === id);
+        item.checked = !item.checked;
+        saveTasks(tasks);
     }
 });
+
 
 
 

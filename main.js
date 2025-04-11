4f3a7deb800a9f0a0615fdde9dca0e4d1b7c2476
vlad <sashkamarichok@gmail.com>
2025-04-11 15:12:27 +0300

    l17hw1

---
+++ main.js
@@ -0,0 +1,32 @@
+class Calculator {
+
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b === 0) {
+            console.warn("На ноль делить нельзя");
+            return NaN;
+        }
+        return a / b;
+    }
+}
+
+const calculator = new Calculator();
+
+console.log(calculator.add(5, 3)); // 8
+
+console.log(calculator.subtract(10, 4)); // 6
+
+console.log(calculator.multiply(3, 6)); // 18
+
+console.log(calculator.divide(8, 0)); // 4
